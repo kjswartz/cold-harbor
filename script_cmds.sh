0a99f467607b850cc55233c85beffe89879ee06e
Kyle Swartz <kjswartz@github.com>
2025-06-16 21:18:38 -0400

    Update script_cmds.sh

--- script_cmds.sh
+++ script_cmds.sh
@@ -1,4 +1,4 @@
 # get lines with repeat occurances 
 awk -F '\t' '{print $3} cargo_volume_at_us_ports.tsv | sort | uniq -d | grep -f - cargo_volume_at_us_ports.tsv
 
-awk -F '\t' '{print $3} cargo_volume_at_us_ports.tsv | sort | uniq -d | grep -f - cargo_volume_at_us_ports.tsv | tail -n +2 | jq -s 'add'
+awk -F '\t' '{print $3} cargo_volume_at_us_ports.tsv | sort | uniq -d | grep -f - cargo_volume_at_us_ports.tsv | tail -n +2 | awk -F '\t' '{print $4}' | jq -s 'add'
