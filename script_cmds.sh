7c57d970011d552d6bd27d98ae8274d8d75fa8ab
Kyle Swartz <kjswartz@github.com>
2025-06-18 10:12:17 -0400

    Update script_cmds.sh

--- script_cmds.sh
+++ script_cmds.sh
@@ -5,4 +5,6 @@ awk -F '\t' '{print $3} cargo_volume_at_us_ports.tsv | sort | uniq -d | grep -f 
 
 awk -F '\t' '{print $3} cargo_volume_at_us_ports.tsv | sort | uniq -d | grep -f - cargo_volume_at_us_ports.tsv | tail -n +2 | sed 's/,//g' | awk -F '\t' '{s+=$4} END '{print s}'
 
-awk -F '\t' '{print $3}' cargo_volume_at_us_ports.tsv | sort | uniq -d | grep -f - cargo_volume_at_us_ports.tsv | sed 's/,//g' | sort -k4 -o tmp-file.tsv
+awk -F '\t' '{print $3}' cargo_volume_at_us_ports.tsv | sort | uniq -d | grep -f - cargo_volume_at_us_ports.tsv | sed 's/,//g' | sort -nr -k4 -t '\t' -o tmp-file.tsv
+
+(head -n 1 tmp-file.csv && tail -n +2 tmp-file.csv | sort -nr -k4 -t ',') > tmp-file-sorted.csv
